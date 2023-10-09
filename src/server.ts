197388c15cad96eece97838b19924a9fa7c80181
Jonata Silva <jonatasilva118@gmail.com>
2023-10-09 16:24:48 -0300

    feat: add cors

--- src/server.ts
+++ src/server.ts
@@ -3,23 +3,42 @@ import { createPerson } from "./routes/CreateUser";
 import { user } from "./routes/User";
 import { login } from "./routes/Login";
 import { apartaments } from "./routes/Apartaments";
+import cors, { FastifyCorsOptions } from "@fastify/cors";
 
-export const server = fastify({logger: true})
+const whiteList = ["http://localhost:5173"];
 
-server.register(createPerson)
-server.register(apartaments)
-server.register(user)
-server.register(login)
+const corsOptions: FastifyCorsOptions = {
+  origin: (origin, callback) => {
+    if ((origin && whiteList.includes(origin)) ?? !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"), false);
+    }
+  },
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+};
 
-server.listen({
-    port: 3333
-}).then((err)=>{
-    console.log('running on http://localhost:3333')
-})
+export const server = fastify({ logger: true });
 
-server.get('/', ()=>{
-    return "Created!"
-})
+server.register(cors, corsOptions)
+
+server.register(createPerson);
+server.register(apartaments);
+server.register(user);
+server.register(login);
+
+server
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log("running on http://localhost:3333");
+  });
+
+server.get("/", () => {
+  return "Created!";
+});
 
 // rota para criar algo ex.: POST localhost:3333/user
 // rota para deletar algo ex.: DELETE localhost:3333/user/del
